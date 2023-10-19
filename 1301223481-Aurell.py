db42e94369f94340388978b15eee147c8a68cb8d
aurellnabila <147801670+aurellnabila@users.noreply.github.com>
2023-10-19 09:22:23 +0700

    commit

---
+++ 1301223481-Aurell.py
@@ -0,0 +1,9 @@
+# Meminta pengguna memasukkan nama
+nama = input("Aurell Nabila Thofansa Putri ")
+
+# Meminta pengguna memasukkan NIK
+nik = input("1301223481 ")
+
+# Mencetak nama dan NIK
+print("Nama: " + nama)
+print("NIK: " + nik)
